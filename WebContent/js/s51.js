d62275ff1c136ac50c49b5fe32949a6b9d8105f3
Manny <egalli64@gmail.com>
2019-07-20 22:30:29 +0200

    JQuery getters and setters

---
+++ WebContent/js/s51.js
@@ -0,0 +1,54 @@
+/**
+ * s51.js
+ * 
+ * Slide 51
+ */
+$(function() {
+    let important = false;
+
+    $('#changeMsg1').click(function() {
+        let msg = $('#msg1');
+        $('#target').html(msg.html());
+        if (important) {
+            msg.html('An <b>important</b> message');
+        } else {
+            msg.html('A <i>plain</i> message');
+        }
+        important = !important;
+    });
+
+    $('#changeTextMsg1').click(function() {
+        let msg = $('#msg1');
+        $('#target').text(msg.text());
+        if (important) {
+            msg.text('An <b>important</b> message');
+        } else {
+            msg.text('A <i>plain</i> message');
+        }
+        important = !important;
+    });
+
+    $('#changeMsg2').click(function() {
+        let msg = $('#msg2');
+        $('#target').text(msg.val());
+        if (important) {
+            msg.val('Important');
+        } else {
+            msg.val('Plain');
+        }
+        important = !important;
+    });
+
+    $('#upsize').click(function() {
+        let msg = $('#msg3');
+        let cur = parseInt(msg.css('font-size'));
+        msg.css('font-size', cur * 2);
+    });
+
+    $('#downsize').click(function() {
+        let msg = $('#msg3');
+        let cur = parseInt(msg.css('font-size'));
+        msg.css('font-size', cur > 1 ? cur / 2 : 1);
+    });
+
+});
