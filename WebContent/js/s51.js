8cf0fa10b45b59e9880849f7c4a183ca714fa435
egalli64 <egalli64@gmail.com>
2019-08-28 11:22:24 +0200

    jquery class methods review

--- WebContent/js/s51.js
+++ WebContent/js/s51.js
@@ -4,15 +4,42 @@
  * Slide 51
  */
 $(function() {
-    $('#colorOn').click(function() {
-        $('#msg1').addClass('red');
-    });
+	$('#colorOn').click(function() {
+		$('#msg1').addClass('red');
+	});
 
-    $('#colorOff').click(function() {
-        $('#msg1').removeClass('red');
-    });
+	$('#colorOff').click(function() {
+		$('#msg1').removeClass('red');
+	});
 
-    $('#swapColor').click(function() {
-        $('#msg2').toggleClass('red');
-    });
+	$('#swapColor').click(function() {
+		$('#msg2').toggleClass('red');
+	});
+
+	$('#makeItRed').click(function() {
+		let cur = $('#msg3');
+
+		cur.removeClass('yellow');
+		cur.addClass('red');
+	});
+
+	$('#makeItYellow').click(function() {
+		let cur = $('#msg3');
+
+		cur.removeClass('red');
+		cur.addClass('yellow');
+	});
+
+	$('#swap3').click(function() {
+		let cur = $('#msg3');
+
+		if (cur.hasClass('red')) {
+			cur.removeClass('red');
+			cur.addClass('yellow');
+		} else if (cur.hasClass('yellow')) {
+			cur.removeClass('yellow');
+		} else {
+			cur.addClass('red');
+		}
+	});
 });
